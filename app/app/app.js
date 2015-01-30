a608a8d6a90470d9efa1d94609373e5df4d47ccb
Akhil Nistala <anistala9@gmail.com>
2015-01-30 15:58:05 -0800

    Copyright

--- app/app/app.js
+++ app/app/app.js
@@ -1,3 +1,11 @@
+/*
+// app.js
+// HackTrack
+//
+// Copyright 2015 (c) Progress Software
+// Author: Akhil Nistala
+*/
+
 var application = require("application");
 application.mainModule = "app/views/frontpage";
 application.start();
